126d5b8bf25ca2f64f855502c31727acff122f9e
Jeremy <jdavid.rupprecht@gmail.com>
2023-11-27 03:23:06 -0700

    Open modal on button click and Close modal popup upon outside click

--- script.js
+++ script.js
@@ -19,7 +19,36 @@ function displayBooks() {
 
 }
 
-// let AddBooks = document.querySelector();
+const addBookButton = document.querySelector('.addBookButton');
+const addBooksModal = document.querySelector('.addBookModal');
+const modalFullscreenBackground = document.querySelector('.modalBackground');
+
+addBookButton.addEventListener('click', () => {
+    addBooksModal.classList.add('showModal');
+    modalFullscreenBackground.style.display = "block";
+    modalFullscreenBackground.style.pointerEvents = "auto";
+});
+
+// Close modal on outside click
+
+document.addEventListener('click', (e) => {
+    if (e.target == modalFullscreenBackground) {
+        addBooksModal.classList.remove('showModal');
+        modalFullscreenBackground.style.display = "none";
+        modalFullscreenBackground.style.pointerEvents = "none";
+    }
+});
+
+
+// document.addEventListener('click', (e) => {
+//     let clickInside = addBooksModal.contains(e.target);
+
+//     if (!clickInside && modalShowing) {
+//         addBooksModal.classList.remove('showModal');
+//         modalShowing = false;
+//         console.log("REEE");
+//     }
+// });
 
 
 // Remove the "Confirm Form Resubmission" prompt on page refresh
