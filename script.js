0d45f535cb72c0dac11b8b8e211ebe186c9565e6
Jeremy <jdavid.rupprecht@gmail.com>
2023-11-26 22:24:04 -0700

    Add Book constructor and basic CSS

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("testing...");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    // this.info = function() {
+    //   return this.read === "yes" ? `${this.title} by ${this.author}, ${this.pages}, read already`
+    //                              : `${this.title} by ${this.author}, ${this.pages}, not read yet`; 
+    // }
+  }
+
+function addBookToLibrary() {
+
+}
